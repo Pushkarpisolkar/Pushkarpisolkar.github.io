88656edac8e0a85dea27225e38af8c6f57fda78e
Pushkarpisolkar <pushkarpisolkar360@gmail.com>
2023-10-01 01:10:08 +0530

    Update script.js

--- Module 4/script.js
+++ Module 4/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[0]="Pushkar";
+names[1]="Bhavik";
+names[2]="Shruti";
+names[3]="Anup";
+names[4]="Rishi";
+names[5]="Rishant";
+names[6]="Yogesh";
+names[7]="Arya";
+names[8]="Komal";
+names[9]="Radhika";
 
 
 for (var i = 0; i < names.length; i++) {
